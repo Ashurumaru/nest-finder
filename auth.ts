fd3bf7ad4ace002ba422e83ee716439c2af5dd44
Ashurumaru <86610969+Ashurumaru@users.noreply.github.com>
2025-03-18 20:00:28 +0800

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -20,14 +20,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     VKProvider({
       clientId: process.env.VK_ID,
       clientSecret: process.env.VK_SECRET,
-      authorization: {
-        url: "https://oauth.vk.com/authorize",
-        params: {
-          scope: "email",
-          response_type: "code",
-          v: "5.131"
-        }
-      },
       checks: ["none"]
     }),
     YandexProvider({
