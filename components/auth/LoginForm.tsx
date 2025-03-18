7d8e81672b8496f4e3f3450f88a2f29b4ea481ca
Ashurumaru <86610969+Ashurumaru@users.noreply.github.com>
2025-03-18 22:50:50 +0800

    Update LoginForm.tsx

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -125,9 +125,22 @@ export const LoginForm = () => {
                 />
                 Продолжить через Yandex
             </a>
-            <div className="mb-3">
-                <VkIdAuthButton callbackUrl={callbackUrl} />
-            </div>
+            <a
+                className='px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3'
+                style={{backgroundColor: '#0077FF'}}
+                onClick={() => signIn('vk', {callbackUrl})}
+                role='button'
+            >
+                <Image
+                    className='pr-2'
+                    src='/images/vk.svg'
+                    alt=''
+                    style={{height: '2rem'}}
+                    width={35}
+                    height={35}
+                />
+                Продолжить через VK
+            </a>
 
             <div className="text-center mt-4 text-xs text-gray-500">
                 Используя сервис, вы соглашаетесь с
